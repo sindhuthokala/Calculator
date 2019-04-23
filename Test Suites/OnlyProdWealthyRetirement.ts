57b8bad0ddae471d6b6b77135ea38eae01726796
sindhuthokala <sindhuthokala94@gmail.com>
2019-04-23 09:51:17 -0400

    Adding a new Test Suite

---
+++ Test Suites/OnlyProdWealthyRetirement.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>OnlyProdWealthyRetirement</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a194e9df-f0e8-4fab-9449-b17e4ab5167c</testSuiteGuid>
+   <testCaseLink>
+      <guid>b54abfe6-f4aa-4b0f-a82b-b5d356fa9331</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Prod_DividendReinvestment</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
